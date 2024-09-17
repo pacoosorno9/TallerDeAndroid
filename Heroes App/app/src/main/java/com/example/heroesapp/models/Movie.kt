7ce5e5e6cfa8bc68ee7e421aed4bd74f16efb1dd
Juan Francisco Osono Martinez <jom77268@lasallebajio.edu.mx>
2024-09-16 21:56:43 -0600

    Agregado de imagenes de movies
    Agregado de imagenes de movies

--- Heroes App/app/src/main/java/com/example/heroesapp/models/Movie.kt
+++ Heroes App/app/src/main/java/com/example/heroesapp/models/Movie.kt
@@ -3,11 +3,11 @@ package com.example.heroesapp.models
 data class Movie(val id: Int,val name:String,val image:String){
     companion object{
         val movie = mutableListOf<Movie>(
-            Movie(1,"Burger King","https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Burger_King_logo_%281999%29.svg/2024px-Burger_King_logo_%281999%29.svg.png"),
-            Movie(2,"McDonald's","https://brandemia.org/contenido/subidas/2022/10/marca-mcdonalds-logo-1024x572.png"),
-            Movie(3,"KFC","https://grains.org/lta/wp-content/uploads/sites/6/2018/01/KFC-logo-MAIN-STORY.png"),
-            Movie(4,"Pizza Hut","https://graffica.info/wp-content/uploads/2017/07/Pizza_Hut_Logo_3.png"),
-            Movie(5,"Subway","https://newsroom.subway.com/download/Subway+Choicemark.png"),
+            Movie(1,"Avengers","https://seeklogo.com/images/A/avengers-logo-5B0A68AFB3-seeklogo.com.png"),
+            Movie(2,"BatMan VS SuperMan","https://i.pinimg.com/originals/84/2c/2c/842c2c2b50fc4baaa85c32b7361ee53c.png"),
+            Movie(3,"Guardianes De La Galaxia","https://i.pinimg.com/originals/fd/0f/7b/fd0f7b7443f0fa6d8ebf346aec421805.png"),
+            Movie(4,"La Liga De La Justicia","https://1000marcas.net/wp-content/uploads/2021/04/Justice-League-Logo-2017.png"),
+            Movie(5,"Civil War","https://upload.wikimedia.org/wikipedia/commons/d/d2/Captain_America_Civil_War_logo.png"),
         )
     }
 }
